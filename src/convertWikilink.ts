c91cddf88333eb581ab982d1d22f8d18204ef9fc
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-01-04 08:09:51 +0900

    Filter out the wikilink to files

--- src/convertWikilink.ts
+++ src/convertWikilink.ts
@@ -3,7 +3,10 @@ import showStatus from "./showStatus";
 
 function findWikiLinks(wikitext: string) {
     let wikiLinkSet = new Set([...wikitext.matchAll(/\[\[(.+?)]]/g)].map(m => m[1])).keys();
-    return [...wikiLinkSet].map(link => link.split('|')).filter(s => s.length <= 2);
+    return [...wikiLinkSet]
+      .filter(link => !link.startsWith('File:'))
+      .map(link => link.split('|'))
+      .filter(s => s.length <= 2);
 }
 
 export async function convertWikilink(wikitext: string) {
